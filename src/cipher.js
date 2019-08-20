fe42cf6b98c3ae8295b931c2f77a88fa06d0b0cc
GeniferGonzalezSantos <genh_@hotmail.com>
2019-08-20 16:24:07 -0300

    adicionar form  input no html e funcao no index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,6 @@
 window.cipher = {
-  // ... 
+  //criar funções e chamar no index html// 
 };
+//function encode(p1,p2){
+   // alert(p1,p2);
+//}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+function clique (text){
+   document.getElementById(caixa1).innerHTML;
+   document.getElementById(caixa2).innerHTML;
+   document.getElementById(btn).value;
+}
