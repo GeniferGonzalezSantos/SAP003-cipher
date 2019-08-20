4baaf708a3b8ee5c84ccdbec6d2b5332925b9519
GeniferGonzalezSantos <genh_@hotmail.com>
2019-08-20 17:56:38 -0300

    adicionar comentarios e comandos

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,6 @@
 window.cipher = {
   //criar funções e chamar no index html// 
 };
-//function encode(p1,p2){
+//function encode(p1 msg,p2 deslocamento){
    // alert(p1,p2);
 //}
--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ function clique (text){
    document.getElementById(caixa2).innerHTML;
    document.getElementById(btn).value;
 }
+//Function pegarEncode()
+//let msg=...get.....value... vai pegar a msg tem que return msg
+//let deslo=...get....value...vai pegar o deslocamento tem que return um number
+//let lala =cipher.escode(msg,desloca)
+//..............innerHTML= lala
