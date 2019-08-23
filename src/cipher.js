a68bc391e2e28e90c657672043d809e331c6a11a
GeniferGonzalezSantos <genh_@gmail.com>
2019-08-23 10:34:41 -0300

    adicionar funcao e loop na funcao

--- src/cipher.js
+++ src/cipher.js
@@ -2,5 +2,17 @@ window.cipher = {
   //criar funções e chamar no index html// 
 };
 //function encode(p1 msg,p2 deslocamento){
-   // alert(p1,p2);
+// alert(p1,p2);
 //}
+
+function code(ABC) {
+  let ABC = "";
+  for (i = 0, i < ABC.lenght, i++) {
+    let numeroletra = ABC.charCodeAt();
+    let valorarmazenado;
+    let valorletracifrada;
+  if (numeroletra <= 65 && numeroletra >= 90) /*para deixar as letras maoúscula*/
+  }
+
+}
+
