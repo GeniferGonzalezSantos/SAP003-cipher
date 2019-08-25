1c08268e61fd208a13fb07f4dba9a1140149fc00
GeniferGonzalezSantos <genh_@gmail.com>
2019-08-25 18:23:51 -0300

    adicionar fincao de decifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,6 @@
 window.cipher = {
-  encode: encode//,
-  //decode: decode
+  encode: encode,
+  decode: decode
 }
 
 function encode(msg, offset) {
@@ -9,22 +9,48 @@ function encode(msg, offset) {
   let valorLetraCifrada;
   for (let i = 0; i < msg.length; i++) {
     let numeroLetra = msg.charCodeAt(i);
-    valorLetraCifrada = String.fromCharCode(formula);
     if (numeroLetra >= 65 && numeroLetra <= 90) {
       formula = (numeroLetra - 65 + offset) % 26 + 65;
-      abc += valorLetraCifrada;
+
     } else if (numeroLetra >= 97 && numeroLetra <= 122) {
       formula = (numeroLetra - 97 + offset) % 26 + 97;
-      abc += valorLetraCifrada;
     } else if (numeroLetra === 32) {
-      abc += " ";
+      formula = 32;
+    }
+    else {
+      alert("Caracter inválido! " + msg[i]);
+      break;
+    }
+    //console.log("valor de abc = " + abc);
+    valorLetraCifrada = String.fromCharCode(formula);
+    abc += valorLetraCifrada;
+  }
+  return abc;
+}
+
+function decode(msg, offset) {
+  let abc = "";
+  let formulaDecifra;
+  let valorLetraDecifra
+  for (let i = 0; i < msg.length; i++) {
+    let numeroLetra = msg.charCodeAt(i);
+    if (numeroLetra >= 65 && numeroLetra <= 90) {
+      formulaDecifra = ((numeroLetra - 65) - offset + 26) % 26 + 65;
+    } else if (numeroLetra >= 97 && numeroLetra <= 122) {
+      formulaDecifra = ((numeroLetra - 97) - offset + 26) % 26 + 97;
+    } else if (numeroLetra === 32) {
+      formulaDecifra = 32
     }
     else {
       alert("Caracter inválido!");
       break;
     }
-    console.log("valor de abc = " + abc);
+    //console.log("valor de abc = " + abc);
+    valorLetraDecifra = String.fromCharCode(formulaDecifra);
+    abc += valorLetraDecifra;
   }
   return abc;
 }
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 
-function clickcode(){
+function clickcode() {
    event.preventDefault();
    let msg = document.getElementById("caixa1").value;
-   let offset = parseInt(document.getElementById("desloc").value);//estava id=number ~ nao existe
-   console.log(msg);
-   console.log(offset);
-   
+   let offset = parseInt(document.getElementById("desloc").value);
+   //console.log(msg);
+   //console.log(offset);
    let encode = window.cipher.encode(msg, offset);
-
-   console.log("retorno do encode " + encode);
-   
+   //console.log("retorno do encode " + encode);
    document.getElementById("msgcode").value = encode;
 }
-//Function pegarEncode()
-//let msg=...get.....value... vai pegar a msg tem que return msg
-//let deslo=...get....value...vai pegar o deslocamento tem que return um number
-//let lala =cipher.encode(msg,desloca)
-//..............innerHTML= lala
+
+function clickDecode() {
+   event.preventDefault();
+   let msg = document.getElementById("msgcode").value;
+   let offset = parseInt(document.getElementById("desloc").value);
+   //console.log(msg);
+   //console.log(offset);
+   let decode = window.cipher.decode(msg, offset);
+   //console.log("retorno do decode " + decode);
+   document.getElementById("caixa2").value = decode;
+}
