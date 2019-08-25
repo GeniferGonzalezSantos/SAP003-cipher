1b6d4891a6fc75066d2e6d8a25c2fadcabb04a21
GeniferGonzalezSantos <genh_@gmail.com>
2019-08-24 21:14:11 -0300

    adicionar funcao de codificar

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,30 @@
 window.cipher = {
-  //criar funções e chamar no index html// 
-};
-//function encode(p1 msg,p2 deslocamento){
-// alert(p1,p2);
-//}
+  encode: encode//,
+  //decode: decode
+}
 
-function cifrar(code) {
-  let ABC = "";
-  for (i = 0, i < ABC.lenght, i++){
-    let numeroletra = ABC.charCodeAt();
-    let formula;
-    let valorletracifrada;
-  }if (numeroletra <= 65 && numeroletra >= 90){ /*para deixar as letras maoúscula*/
-     formula = (numeroletra - 65 + /*offset*/)% 26 - 65;
-     valorletracifrada = String.fromCharCode(formula);
-    ABC += valorletracifrada; //para cifrar//
-  }else if(numerodaletra <= 97 && numeroletra >= 122){
-    formula = (numeroletra - 97 + /*offset*/)% 26 - 97;
-    valorletracifrada += String.fromCharCode()
+function encode(msg, offset) {
+  let abc = "";
+  let formula;
+  let valorLetraCifrada;
+  for (let i = 0; i < msg.length; i++) {
+    let numeroLetra = msg.charCodeAt(i);
+    valorLetraCifrada = String.fromCharCode(formula);
+    if (numeroLetra >= 65 && numeroLetra <= 90) {
+      formula = (numeroLetra - 65 + offset) % 26 + 65;
+      abc += valorLetraCifrada;
+    } else if (numeroLetra >= 97 && numeroLetra <= 122) {
+      formula = (numeroLetra - 97 + offset) % 26 + 97;
+      abc += valorLetraCifrada;
+    } else if (numeroLetra === 32) {
+      abc += " ";
     }
-
-
-
-
-  
-
+    else {
+      alert("Caracter inválido!");
+      break;
+    }
+    console.log("valor de abc = " + abc);
+  }
+  return abc;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 
-function clique (text){
-   document.getElementById(caixa1).innerHTML;
-   document.getElementById(caixa2).innerHTML;
-   document.getElementById(btn).value;
+function clickcode(){
+   event.preventDefault();
+   let msg = document.getElementById("caixa1").value;
+   let offset = parseInt(document.getElementById("desloc").value);//estava id=number ~ nao existe
+   console.log(msg);
+   console.log(offset);
+   
+   let encode = window.cipher.encode(msg, offset);
+
+   console.log("retorno do encode " + encode);
+   
+   document.getElementById("msgcode").value = encode;
 }
 //Function pegarEncode()
 //let msg=...get.....value... vai pegar a msg tem que return msg
 //let deslo=...get....value...vai pegar o deslocamento tem que return um number
-//let lala =cipher.escode(msg,desloca)
+//let lala =cipher.encode(msg,desloca)
 //..............innerHTML= lala
