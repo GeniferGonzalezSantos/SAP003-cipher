e169675741445e7bb9c15ae9009cc5617862d2a5
GeniferGonzalezSantos <genh_@gmail.com>
2019-08-27 20:18:48 -0300

    adicionar caracteres do hacker edition

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
 window.cipher = {
   encode: encode,
   decode: decode
-}
+};
 
 function encode(msg, offset) {
   let abc = "";
@@ -11,60 +11,40 @@ function encode(msg, offset) {
     let numeroLetra = msg.charCodeAt(i);
     if (numeroLetra >= 65 && numeroLetra <= 90) {
       formula = (numeroLetra - 65 + offset) % 26 + 65;
+      valorLetraCifrada = String.fromCharCode(formula);
     } else if (numeroLetra >= 97 && numeroLetra <= 122) {
       formula = (numeroLetra - 97 + offset) % 26 + 97;
-    } else if (numeroLetra >= 33 && numeroLetra <= 47) {
-      formula = (numeroLetra - 33 + offset) % 26 + 47;
-    } else if (numeroLetra >= 58 && numeroLetra <= 64) {
-      formula = (numeroLetra - 58 + offset) % 26 + 58;
-    } else if (numeroLetra >= 91 && numeroLetra <= 96) {
-      formula = (numeroLetra - 91 + offset) % 26 + 91;
-    //} else if (numeroLetra >= 128 && numeroLetra <= 136) {
-    //formula = (numeroLetra - 128 + offset) % 26 + 128;
-  } else if (numeroLetra === 32) {
-    formula = 32;
+      valorLetraCifrada = String.fromCharCode(formula);
+    }
+    else {
+      valorLetraCifrada = msg[i];
+    }
+    abc += valorLetraCifrada;
+    //console.log("valor de abc = " + abc);
+    //valorLetraCifrada = String.fromCharCode(formula);
   }
-  else {
-    alert("Caracter inválido! " + msg[i]);
-    break;
-  }
-  //console.log("valor de abc = " + abc);
-  valorLetraCifrada = String.fromCharCode(formula);
-  abc += valorLetraCifrada;
-}
-return abc;
+  return abc;
 }
 
 function decode(msg, offset) {
   let abc = "";
   let formulaDecifra;
-  let valorLetraDecifra
+  let valorLetraDecifra;
   for (let i = 0; i < msg.length; i++) {
     let numeroLetra = msg.charCodeAt(i);
     if (numeroLetra >= 65 && numeroLetra <= 90) {
       formulaDecifra = ((numeroLetra - 65) - offset + 26) % 26 + 65;
+      valorLetraDecifra = String.fromCharCode(formulaDecifra);
     } else if (numeroLetra >= 97 && numeroLetra <= 122) {
       formulaDecifra = ((numeroLetra - 97) - offset + 26) % 26 + 97;
-    } else if (numeroLetra >= 33 && numeroLetra <= 47) {
-      formulaDecifra = ((numeroLetra - 33) - offset + 14) % 14 + 33;
-    /*} else if (numeroLetra >= 58 && numeroLetra <= 64) {
-      formulaDecifra = ((numeroLetra - 58) - offset + 26) % 26 + 58;
-    } else if (numeroLetra >= 91 && numeroLetra <= 96) {
-      formulaDecifra = ((numeroLetra - 91) - offset + 26) % 26 + 91;
-    //} else if (numeroLetra >= 128 && numeroLetra <= 136) {
-    //formulaDecifra = ((numeroLetra - 128) - offset + 136) % 26 + 128;*/
-    } else if (numeroLetra === 32) {
-    formulaDecifra = 32
+      valorLetraDecifra = String.fromCharCode(formulaDecifra);
+    }
+    else {
+      valorLetraDecifra = msg[i];
+    }
+    abc += valorLetraDecifra;
   }
-  else {
-    alert("Caracter inválido!");
-    break;
-  }
-  //console.log("valor de abc = " + abc);
-  valorLetraDecifra = String.fromCharCode(formulaDecifra);
-  abc += valorLetraDecifra;
-}
-return abc;
+  return abc;
 }
 
 
