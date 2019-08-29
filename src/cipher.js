e957e42908eeec5852790edbb740c2dc41babb77
GeniferGonzalezSantos <genh_@hotmail.com>
2019-08-29 16:15:06 -0300

    adicionar deslocamento negativo

--- src/cipher.js
+++ src/cipher.js
@@ -10,10 +10,10 @@ function encode(offset, msg) {
   for (let i = 0; i < msg.length; i++) {
     let numeroLetra = msg.charCodeAt(i);
     if (numeroLetra >= 65 && numeroLetra <= 90) {
-      formula = (numeroLetra - 65 + offset) % 26 + 65;
+      formula = (((numeroLetra - 65) + (offset % 26 + 26)) % 26 + 65);
       valorLetraCifrada = String.fromCharCode(formula);
     } else if (numeroLetra >= 97 && numeroLetra <= 122) {
-      formula = (numeroLetra - 97 + offset) % 26 + 97;
+      formula = (((numeroLetra - 97) + (offset % 26 + 26)) % 26 + 97);
       valorLetraCifrada = String.fromCharCode(formula);
     }
     else {
@@ -31,10 +31,10 @@ function decode(offset, msg) {
   for (let i = 0; i < msg.length; i++) {
     let numeroLetra = msg.charCodeAt(i);
     if (numeroLetra >= 65 && numeroLetra <= 90) {
-      formulaDecifra = ((numeroLetra - 65) - offset + 26) % 26 + 65;
+      formulaDecifra = (((numeroLetra - 65) + (- offset % 26 + 26)) % 26 + 65);
       valorLetraDecifra = String.fromCharCode(formulaDecifra);
     } else if (numeroLetra >= 97 && numeroLetra <= 122) {
-      formulaDecifra = ((numeroLetra - 97) - offset + 26) % 26 + 97;
+      formulaDecifra = (((numeroLetra - 97) + (- offset % 26 + 26)) % 26 + 97);
       valorLetraDecifra = String.fromCharCode(formulaDecifra);
     }
     else {
