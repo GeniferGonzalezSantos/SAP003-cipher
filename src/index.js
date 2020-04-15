25dc19599aa0a2efc53e225bdb193a9b52575ec7
GeniferGonzalezSantos <51093689+GeniferGonzalezSantos@users.noreply.github.com>
2020-04-15 17:44:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.getElementById("btnDecode").addEventListener("click", clickDecode);
 document.getElementById("btnCode").addEventListener("click", clickCode);
 
-function clickCode() {
+function clickCode(event) {
   event.preventDefault();
   let msg = document.getElementById("entradaTexto").value;
   let offset = parseInt(document.getElementById("desloc").value);
@@ -9,7 +9,7 @@ function clickCode() {
   document.getElementById("saidaTexto").value = encode;
 }
 
-function clickDecode() {
+function clickDecode(event) {
   event.preventDefault();
   let msg = document.getElementById("entradaTexto").value;
   let offset = parseInt(document.getElementById("desloc").value);
